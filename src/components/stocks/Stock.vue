d6d79859ca9dac713e29ba032864823b57029cfe
Jasmine <jastangting@gmail.com>
2019-03-11 09:35:32 +0800

    added vuex

---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="col-sm-4 col-md-12">
+    <div class="panel panel-success">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          {{stock.name}}
+          <small>(Price: {{stock.price}})</small>
+        </h3>
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <input
+                type="number"
+                class="form-control"
+                placeholder="Quantity"
+                v-model="quantity"
+                >
+        </div>
+        <div class="pull-right">
+          <button
+                class="btn btn-success"
+                @click="buyStock"
+                :disabled="quantity <=0 || Number.isInteger(quantity)"
+                >Buy</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['stock'],
+    data() {
+      return {
+        quantity: 0
+      }
+    },
+    methods: {
+      buyStock() {
+        const order = {
+          stockId: this.stock.id,
+          stockPrice: this.stock.price,
+          quantity: this.quantity
+        }
+        console.log(Number.isInteger(this.quantity));
+        this.quantity = 0
+      }
+    }
+  }
+</script>
---
+++ src/data/stocks.js
@@ -0,0 +1,6 @@
+export default [
+  { id: 1, name: 'Yoga', price: 110 },
+  { id: 2, name: 'Basic Pass', price: 200 },
+  { id: 3, name: 'Universal Pass', price: 250 },
+  { id: 4, name: 'Annual Swim', price: 100 }
+]
